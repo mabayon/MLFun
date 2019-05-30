86168a4f857e213dd76dbc9b8000bbc2510d03b2
mabayon <mabayon@student.42.fr>
2019-05-30 22:24:57 +0200

    Add to GitHub

--- MLFun/ViewController.swift
+++ MLFun/ViewController.swift
@@ -7,19 +7,70 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var confidenceLabel: UILabel!
+    
+    let resnetModel = Resnet50()
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
+        
+        if let image = imageView.image {
+            processImage(image)
+        }
+    }
+    
+    func processImage(_ image: UIImage) {
+        if let model = try? VNCoreMLModel(for: self.resnetModel.model) {
+            
+            let request = VNCoreMLRequest(model: model, completionHandler: { (request, error) in
+                
+                if let results = request.results as? [VNClassificationObservation] {
+                    for classification in results {
+                        print("ID: \(classification.identifier) Confidence: \(classification.confidence)")
+                    }
+                    self.descriptionLabel.text = results.first?.identifier
+                  
+                    if let confidence = results.first?.confidence {
+                        self.confidenceLabel.text = "\(confidence * 100.0)%"
+                    }
+                }
+            })
+            
+            if let imageData = UIImagePNGRepresentation(image) {
+                
+                let handler = VNImageRequestHandler(data: imageData, options: [:])
+                try? handler.perform([request])
+            }
+        }
+    }
+    
+    @IBAction func photosTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func cameraTapped(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
     }
-
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let imageSelected = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = imageSelected
+            processImage(imageSelected)
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
 }
 
